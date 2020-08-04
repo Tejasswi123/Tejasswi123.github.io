8ba1d63344f25aff5d6901c7f70ff9ae38cce9f1
Tejasswi123 <66202226+Tejasswi123@users.noreply.github.com>
2020-08-04 19:42:52 +0530

    Add files via upload

---
+++ two sum.py
@@ -0,0 +1,12 @@
+class solution():
+  def twoSum(self,nums,target):
+    dict={}
+    for i in range(len(sums)):
+      if nums[i] in dict:
+        return [dict[nums[i],i]
+      else
+         dict[target-nums[i]]=i
+input_list=[2,7,9,15]
+ob1=solution()
+print(ob1.twoSum(input_list,20))
+    
